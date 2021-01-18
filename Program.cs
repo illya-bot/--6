5b891bfbb1696838f6e844db7f663b013b0f7a8c
illya strichevsky <illya.lalka1@gmail.com>
2021-01-18 12:50:56 +0300

    .NET Framework

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Практична робота 6");
+            Console.ReadKey();
         }
     }
 }
